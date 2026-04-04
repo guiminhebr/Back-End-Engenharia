f4085b4bec18104ab8fc6c345753fa329d9676c7
unknown <guilherme.comanini@hotmail.com>
2026-04-03 21:31:40 -0300

    atualização com upload de arquivos

--- src/main/java/Projeto/Engenharia/Engenharia/EngenhariaApplication.java
+++ src/main/java/Projeto/Engenharia/Engenharia/EngenhariaApplication.java
@@ -1,13 +1,25 @@
 package Projeto.Engenharia.Engenharia;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EngenhariaApplication {
+import Projeto.Engenharia.Engenharia.Service.FilesStorageService;
+import jakarta.annotation.Resource;
 
+@SpringBootApplication
+public class EngenhariaApplication implements CommandLineRunner {
+	  @Resource
+	  FilesStorageService storageService;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EngenhariaApplication.class, args);
 	}
 
+	 @Override
+	  public void run(String... arg) throws Exception {
+//	    storageService.deleteAll();
+	    storageService.init();
+	  }
 }
---
+++ src/main/java/Projeto/Engenharia/Engenharia/Exception/FileUploadExceptionAdvice.java
@@ -0,0 +1,21 @@
+package Projeto.Engenharia.Engenharia.Exception;
+
+
+
+
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import Projeto.Engenharia.Engenharia.Controller.ResponseMessage;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice {
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	  public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+	    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
+	  }
+}
